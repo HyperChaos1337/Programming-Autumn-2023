98b012ab096b810fcc6f271413f0116092131c3d
Archangel Tyrael <90914809+HyperChaos1337@users.noreply.github.com>
2023-10-02 12:48:45 +0300

    v1.11: Stack Change

--- lab1/MyStack.java
+++ lab1/MyStack.java
@@ -1,16 +1,3 @@
-/*
-class Node<T>{
-
-    public T field;
-    public Node next;
-
-    public Node(T field){
-        this.next = null;
-        this.field = field;
-    }
-
-}
-*/
 public class MyStack<T>{
 
     DynamicArray<T> array;
@@ -45,50 +32,5 @@ public class MyStack<T>{
     void print(){
         array.print();
     }
-
-    /*
-    Node head, tail;
-
-    void push(T field){
-        Node ptr = new Node(field);
-        if(head == null) head = ptr;
-        if(tail != null) tail.next = ptr;
-        tail = ptr;
-    }
-
-    void pop(){
-        if(tail == null) return;
-        if(head == tail){
-            System.gc();
-            head = tail = null;
-            return;
-        }
-        Node node = head;
-        for(;node.next != tail; node = node.next);
-        node.next = null;
-        System.gc();
-        tail = node;
-    }
-
-    T peek(){
-        if(tail == null){
-            System.out.print("Стек пуст");
-            return null;
-        }
-        return (T) tail.field;
-    }
-
-    void print(){
-        for(Node node = head; node != null; node = node.next){
-            System.out.print(node.field);
-            System.out.print(" ");
-        }
-    }
-
-    int size(){
-        int k = 0;
-        for(Node node = head; node != null; node = node.next) k++;
-        return k;
-    }
-     */
+    
 }
