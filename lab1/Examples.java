68904aa7d9cba326a6bf6903081058c8b2ebbf81
Archangel Tyrael <90914809+HyperChaos1337@users.noreply.github.com>
2023-10-06 10:27:51 +0300

    Delete lab1 directory

--- lab1/Examples.java
+++
@@ -1,5 +0,0 @@
-public class Examples {
-    public void init(){
-
-    }
-}
--- lab1/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(ShuntingYard.shunting_yard("3 + 4 * 2 / ( 1 - 5 ) ^ 2"));
-        System.out.println(ShuntingYard.shunting_yard("sin ( 1 / 2 * 5 )"));
-    }
-}
